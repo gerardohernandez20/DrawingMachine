f4e73936c5c9fe2f85cfa61091581312f62e5a5f
gerardohernandez20 <42582681+gerardohernandez20@users.noreply.github.com>
2021-04-13 19:27:28 -0700

    Draw1

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let array =[];
+
+function setup() {
+  createCanvas(600,600);
+  background(100);
+  strokeWeight(4);
+  noFill();
+}
+
+function draw() {
+if (mouseIsPressed){
+  //stroke(map(mouseX,0,234,0,255,true))
+//line(mouseX, mouseY, pmouseX,pmouseY);
+array.push([mouseX,mouseY]);
+}
+}
+function keyTyped(){
+if (key == 's'){
+  saveCanvas('fileName','png');
+} else if (key === 'd'){
+  //display image
+  background(100);
+  beginShape();
+
+  console.log(array);
+  for(let i = 0; i < array.length-1; i++){
+//line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
+curveVertex(array[i][0], array[i][1]);
+  }
+endShape();
+}
+
+return false;
+
+}
